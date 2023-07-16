5c055b99f32035561a2d54e1639014d4d81c8706
PetrTech <petrne09@gmail.com>
2023-07-16 19:30:21 +0200

    OS-RP

--- OS-RP/OS-RP.cpp
+++ OS-RP/OS-RP.cpp
@@ -1,12 +1,227 @@
-﻿// OS-RP.cpp : Defines the entry point for the application.
-//
-
-#include "OS-RP.h"
+﻿#include "OS-RP.h"
+#include <iostream>
+#include <fstream>
+#include <discord.h>
+#include <chrono>
+#include <thread>
+#include <csignal>
 
 using namespace std;
 
+discord::Core* core{};
+
+volatile sig_atomic_t g_shouldExit = 0;
+int usageTime = 0; // in seconds
+
+// APPLE AND LINUX NOT TESTED!!!!! IF THERE ARE ANY ISSUES, YOU CAN CREATE A PR
+#ifdef _WIN32
+#include <Windows.h>
+const char* platform = "windows";
+
+// Possibly incorrect.
+std::string getWindowsVersion() {
+	OSVERSIONINFO osvi;
+	ZeroMemory(&osvi, sizeof(OSVERSIONINFO));
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+
+	if (GetVersionEx(&osvi)) {
+		if (osvi.dwMajorVersion == 10 && osvi.dwMinorVersion == 0)
+			return "Windows 10";
+		else if (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 3)
+			return "Windows 8.1";
+		else if (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 2)
+			return "Windows 8";
+		else if (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 1)
+			return "Windows 7";
+		else if (osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 0)
+			return "Windows Vista";
+		else if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 2)
+			return "Windows XP 64-bit Edition";
+		else if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 1)
+			return "Windows XP";
+		else if (osvi.dwMajorVersion == 5 && osvi.dwMinorVersion == 0)
+			return "Windows 2000";
+	}
+
+	return "Unknown";
+}
+#elif __linux__
+#include <sys/utsname.h>
+
+const char* platform = "linux";
+
+std::string getLinuxDistroVersion() {
+	struct utsname systemInfo;
+	if (uname(&systemInfo) == 0) {
+		std::string version(systemInfo.release);
+		return "Linux " + version;
+	}
+	return "Unknown";
+}
+
+#elif __APPLE__
+#include <sys/utsname.h>
+
+const char* platform = "macos";
+
+std::string getMacOSVersion() {
+	struct utsname systemInfo;
+	if (uname(&systemInfo) == 0) {
+		std::string version(systemInfo.release);
+		return "macOS " + version;
+	}
+	return "Unknown";
+}
+#else
+const char* platform = "osrp";
+#endif
+
+std::string getLinuxDistroVersion() {
+    std::ifstream osReleaseFile("/etc/os-release");
+    std::string line;
+    
+    while (std::getline(osReleaseFile, line)) {
+        if (line.find("PRETTY_NAME") != std::string::npos) {
+            size_t start = line.find_first_of("\"");
+            size_t end = line.find_last_of("\"");
+            if (start != std::string::npos && end != std::string::npos)
+                return line.substr(start + 1, end - start - 1);
+        }
+    }
+    
+    return "Unknown";
+}
+
+// Signal handler for SIGINT
+void handleSignal(int signal) {
+	if (signal == SIGINT) {
+		g_shouldExit = 1;
+		std::cout << "Caught SIGINT, cleaning up and exiting." << std::endl;
+	}
+}
+
+void clear() {
+	core->ActivityManager().ClearActivity([](discord::Result result) {
+		if (result == discord::Result::Ok) {
+			std::cout << "Cleared" << std::endl;
+		}
+		else {
+			std::cout << "Failed to clear" << std::endl;
+		}
+	});
+}
+
+std::string formatTime(int seconds) {
+	int days = seconds / (60 * 60 * 24);
+	seconds %= (60 * 60 * 24);
+
+	int hours = seconds / (60 * 60);
+	seconds %= (60 * 60);
+
+	int minutes = seconds / 60;
+	seconds %= 60;
+
+	std::ostringstream oss;
+	oss << days << "d" << hours << "h" << minutes << "min" << seconds << "s";
+
+	return oss.str();
+}
+
+void setActivity() {
+	discord::Activity activity{};
+
+	std::string osVer = "No Details";
+
+#ifdef _WIN32
+	osVer = getWindowsVersion();
+	HWND focusedWindow = GetForegroundWindow();
+	char windowTitle[256];
+
+	GetWindowTextA(focusedWindow, windowTitle, sizeof(windowTitle));
+
+	if (currentAppDisplay != -1) {
+		activity.SetDetails(windowTitle);
+	}
+#elif __linux__
+	osVer = getLinuxDistroVersion();
+#elif __APPLE__
+	osVer = getMacOSVersion();
+#endif
+
+	if (displayOSRPTime != -1) {
+		activity.SetState(("OSRP time: " + formatTime(usageTime)).c_str());
+	}
+
+	if (overrideOS == -1) {
+		activity.GetAssets().SetLargeText(osVer.c_str());
+	}
+	else if (overrideOS == 0) {
+		platform = "windows";
+	}
+	else if (overrideOS == 1) {
+		platform = "linux";
+	}
+	else if (overrideOS == 2) {
+		platform = "macos";
+	}
+	else {
+		platform = "osrp";
+	}
+	
+	activity.GetAssets().SetLargeImage(platform);
+
+	if (supportOSRP != -1) {
+		activity.GetAssets().SetSmallImage("osrp");
+		activity.GetAssets().SetSmallText("https://github.com/PetrTech/OS-RP");
+	}
+
+	core->ActivityManager().UpdateActivity(activity, [](discord::Result result) {});
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	try {
+		using namespace std::this_thread;
+		using namespace std::chrono;
+
+		std::signal(SIGINT, handleSignal);
+
+		FILE* cfg = cfgFileExists();
+
+		auto result = discord::Core::Create(1130163445740863508, DiscordCreateFlags_Default, &core);
+		discord::Activity activity{};
+
+		setActivity();
+
+		cout << "Running, keep alive.";
+
+		int loop = 0;
+		while (!g_shouldExit) {
+			try {
+				// Your main loop logic here
+				if (loop == 30) {
+					loop = 0;
+					usageTime++;
+				}
+
+				sleep_for(milliseconds(33));
+				setActivity();
+				loop++;
+				::core->RunCallbacks();
+			}
+			catch (const std::exception& e) {
+				std::cerr << "Exception caught: " << e.what() << std::endl;
+				clear();
+				g_shouldExit = 1;
+			}
+		}
+	}
+	catch (const std::exception& e) {
+		clear();
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
+
+	clear();
+
 	return 0;
 }
--- OS-RP/OS-RP.h
+++ OS-RP/OS-RP.h
@@ -1,8 +1,96 @@
-﻿// OS-RP.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-// TODO: Reference additional headers your program requires here.
+// Config
+int overrideOS = -1; // Keep at -1 to display your OS, otherwise use: 0 - Windows
+					 //												  1 - Linux			    (General)
+					 //												  2 - MacOS
+					 //												  3 - Do not display     (hides your OS and replaces it with: OS-RP)
+
+int currentAppDisplay = 0; // Changes how your current app is shown: -1 - Do not display    (doesn't show your current application)
+						   //										 0  - Default		    (Title only)
+
+int displayOSRPTime = 0; // Changes OSRP time display: -1 - Do not display
+						 //							   0  - Default (Display)
+
+int supportOSRP = 0; // This option allows you to support OS-RP by adding a link to the repository: -1 - Do not display
+					 //																			    0  - Support (Will add a small image with the link in the text)
+
+
+// Code
+// READ FILE LINE-BY-LINE AND SET VARIABLES CORRECTLY
+void readCFG() {
+	std::ifstream file("osrp.txt");
+
+	int lI = 0;
+	for (std::string line; getline(file, line); )
+	{
+		switch (lI) {
+		case 1: {
+			overrideOS = std::stoi(line);
+			break;
+		}
+		case 2: {
+			currentAppDisplay = std::stoi(line);
+			break;
+		}
+		case 3: {
+			displayOSRPTime = std::stoi(line);
+		}
+		case 4: {
+			supportOSRP = std::stoi(line);
+		}
+		}
+		lI++;
+	}
+}
+
+// CHECK CONFIG FILE, OTHERWISE CREATE
+FILE* exists() {
+	FILE* cfg;
+	if (cfg = fopen("osrp.txt", "r")) {
+		std::cout << "Configuration file exists! Reading configuration...\n";
+		fclose(cfg);
+		readCFG();
+		return cfg;
+	}
+	else {
+		std::cout << "Configuration doesn't exist, creating!\n";
+		return NULL;
+	}
+}
+
+// CREATE CONFIG
+FILE* createCfg() {
+	std::fstream file;
+	std::ofstream { "osrp.txt" };
+	file.open("osrp.txt");
+
+	if (!file) {
+		std::cout << "File error!\n";
+	}
+	else {
+		std::cout << "Configuration created! [osrp.txt]\n";
+		file << "*OS-RP CONFIG* -- CHECK GITHUB FOR EXPLANATION -- OR READ THE CODE IN OS-RP.H\n"
+			"-1\n"
+			"0\n"
+			"0\n"
+			"0";
+		file.close();
+	}
+
+	return exists();
+}
+
+// SECOND METHOD TO CHECK WHETHER CONFIG EXISTS
+FILE* cfgFileExists() {
+	FILE* file = exists();
+	if (file == NULL) {
+		file = createCfg();
+	}
+	return file;
+}
